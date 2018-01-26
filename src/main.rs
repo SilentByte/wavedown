758e00ffd1ab890dbee58917ec6d9d2e08f93e24
Rico A. Beti <rico.beti@silentbyte.com>
2018-01-26 17:05:33 +0800

    Setup basic CLI using clap

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,30 @@
 /// <https://github.com/SilentByte/wavedown>
 ///
 
+#[macro_use]
+extern crate clap;
+
+use clap::App;
+use clap::Arg;
+
 fn main() {
-    //
+    let matches = App::new("wavedown")
+        .version("1.0")
+        .about("Transforms a stream of raw PCM 16bit LE data into \
+                a fixed-width waveform representation.")
+        .author("Rico A. Beti <rico.beti@silentbyte.com>")
+        .arg(Arg::with_name("INPUT")
+            .index(1)
+            .help("Sets the input file")
+            .long("input")
+            .short("i")
+            .takes_value(true)
+            .default_value("-"))
+        .arg(Arg::with_name("SAMPLES")
+            .help("Sets the number of samples to output")
+            .long("samples")
+            .short("s")
+            .required(true)
+            .takes_value(true))
+        .get_matches();
 }
